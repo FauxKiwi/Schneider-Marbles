a788410fdf84a9703a070f9c6c0b86e500f785cb
FauxKiwi <sfn14ok04@gmail.com>
2021-11-11 16:48:52 +0100

    Added bar chart

--- Assets/ScoreText.cs
+++ Assets/ScoreText.cs
@@ -9,6 +9,7 @@ public class ScoreText : MonoBehaviour
 
     private int _num;
     private int[] _scores;
+    private int _scoreSum;
 
     private readonly List<GameObject> _instantiated = new List<GameObject>();
     
@@ -16,8 +17,9 @@ public class ScoreText : MonoBehaviour
 
     public void SetNum(float num)
     {
-        _num = ((int) num - 2) * 2;
+        _num = (int) num * 2 - 2;
         _scores = new int[_num];
+        _scoreSum = 0;
 
         foreach (var instance in _instantiated)
         {
@@ -25,23 +27,47 @@ public class ScoreText : MonoBehaviour
         }
         _instantiated.Clear();
 
-        _instantiated.Add(Instantiate(prefab, new Vector3(0, -2, 0), Rotation));
+        for (var i = 0; i < _num / 2; ++i) {
+            _instantiated.Add(Instantiate(prefab, new Vector3(i * 2 + 1, -2, 0), Rotation));
+            _instantiated.Add(Instantiate(prefab, new Vector3(i * -2 - 1, -2, 0), Rotation));
+        }
     }
 
     public void AddScore(float x)
     {
         var index = (int) ((x + _num - 0.5f) / 2);
         ++_scores[index];
+        ++_scoreSum;
 
-        var sb = new StringBuilder();
+        for (int i = 0; i < _scores.Length; ++i) {
+            var barIndex = i >= _num / 2 ? (i - _num / 2) * 2 + 1 : -(i - _num / 2) * 2;
+            --barIndex;
+
+            var o = _instantiated[barIndex];
+
+            var score = _scores[i];
+            var maxSize = _num + 2;
+            var size = score * maxSize / (float) _scoreSum;
+
+            var t = o.transform;
+            t.position = new Vector3(t.position.x, -2 + size / 2f - maxSize, t.position.z);
+            t.localScale = new Vector3(t.localScale.x, size, t.localScale.z);
+
+            /*var text = o.GetComponent<TextMesh>();
+            if (text == null) text = o.AddComponent<TextMesh>();
+            text.text = "new text set";
+            text.fontSize = 30;*/
+        }
+
+        /*var sb = new StringBuilder();
         //var i = -_num / 2;
         foreach (var score in _scores)
         {
             //if (i == 0) ++i;
-            sb.Append(score * 2);
+            sb.Append(score);
             sb.Append(" ");
-        }
+        }*/
 
-        GetComponent<UnityEngine.UI.Text>().text = sb.ToString();
+        GetComponent<UnityEngine.UI.Text>().text = $"({_scoreSum} marbles)";//sb.ToString();
     }
 }
